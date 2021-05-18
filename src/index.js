e6b292463c29ae1ad6713d30b08a3d3887f17da1
avadhut <avadhutprabhudesai23@gmail.com>
2021-05-18 17:50:17 +0530

    feat: implemented expanding cards UI
    Added HTML for cards. Added functionality for card expansion using JS DOM.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './sass/main.scss';
+
+const container = document.querySelector('.container');
+
+const defaultActivePanel = document.querySelector('.panel--1');
+defaultActivePanel.classList.add('active');
+
+container.addEventListener('click', function (event) {
+  const target = event.target;
+
+  if (target.classList.contains('panel')) {
+    const panels = document.querySelectorAll('.panel');
+
+    for (let panel of panels) {
+      if (panel === target) {
+        panel.classList.toggle('active');
+      } else {
+        panel.classList.remove('active');
+      }
+    }
+  }
+});
